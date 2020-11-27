a48ce33e718b5bc7aa671e90baa31c44df72d7b2
shivaluma <shivaluma@gmail.com>
2020-11-28 00:58:23 +0700

    Migrate to Material UI

---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import { Typography } from '@material-ui/core';
+import { ThemeProvider } from '@material-ui/styles';
+import theme from '../../theme';
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Typography variant="h3">Hello</Typography>
+    </ThemeProvider>
+  );
+}
+
+export default App;
